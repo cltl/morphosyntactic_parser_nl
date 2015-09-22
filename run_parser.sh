807e938ce4ebb71afd9d7c7f42d9d9ac5f98a184
Ruben Izquierdo <r.izquierdobevia@vu.nl>
2015-09-22 14:42:23 +0200

    Included examples in run_parser.sh

--- run_parser.sh
+++ run_parser.sh
@@ -3,3 +3,9 @@
 this_dir=$(dirname $0)
 
 python $this_dir/core/morph_syn_parser.py
+
+#For instance to set the maximum number of minutes per sentence to 5 minutes:
+###python $this_dir/core/morph_syn_parser.py -t 5
+
+#Maximum 30 seconds per setence:
+###python $this_dir/core/morph_syn_parser.py -t 0.5
