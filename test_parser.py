896e6efc090c7474a26db97f4bf630eec92bff97
Ruben Izquierdo <r.izquierdobevia@vu.nl>
2016-04-06 13:38:36 +0200

    Included check for ALPINO_HOME variable in the automatic test script

--- test_parser.py
+++ test_parser.py
@@ -1,13 +1,22 @@
-
 from nose.tools import assert_equal
 from KafNafParserPy import KafNafParser
-import os, sys
+import os
 from subprocess import Popen, PIPE
+from unittest import SkipTest
 
 
 __here__ = os.path.dirname(os.path.realpath(__file__))
 
-os.environ['ALPINO_HOME'] = '/home/izquierdo/tools/Alpino'
+
+def set_up_alpino():
+    ##Uncomment next line and point it to your local path to Alpino if you dont want to set the environment variable ALPINO_HOME
+    #os.environ['ALPINO_HOME'] = '/home/izquierdo/tools/Alpino'
+
+    if 'ALPINO_HOME' not in os.environ:
+        raise SkipTest('ALPINO_HOME variable not set. Set this variable to point to your local path to Alpino. For instance:\nexport ALPINO_HOME=/home/your_user/your_tools/Alpino')
+    os.environ['SP_CSETLEN']='212'
+    os.environ['SP_CTYPE']='utf8'
+
 
 def _test_file(this_file):
     input_fd = open(this_file)
@@ -37,12 +46,13 @@ def _test_file(this_file):
     assert_equal(dependencies[5].get_function(),'hd/su')
     
     
-def test_morphosyn_kaf():
+def test_morphosyn():
+    set_up_alpino()
     kaf_file = os.path.join(__here__,'examples','file1.in.kaf')
     _test_file(kaf_file)
     
-    
-def test_morphosyn_naf():
     naf_file = os.path.join(__here__,'examples','file1.in.naf')
     _test_file(naf_file)
+
+
    
